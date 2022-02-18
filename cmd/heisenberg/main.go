ff453731a29970e87f37b2be2679c5ed8dc7d744
Andrew Snodgrass <git@pointlander.info>
2022-02-17 19:50:45 -0600

    Added matrix benchmark

--- cmd/heisenberg/main.go
+++ cmd/heisenberg/main.go
@@ -5,6 +5,10 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
+	"time"
+
 	"github.com/pointlander/heisenberg"
 )
 
@@ -13,4 +17,44 @@ func main() {
 		[2][]float64{[]float64{0, 1}, []float64{1, 0}},
 		[2][]float64{[]float64{1, 0}, []float64{0, 1}},
 	})
+
+	rnd := rand.New(rand.NewSource(1))
+	for i := .1; i <= 1.0; i += .1 {
+		start := time.Now()
+		for j := 0; j < 256; j++ {
+			matrix := heisenberg.Sparse128{
+				R:      256,
+				C:      256,
+				Matrix: make([]map[int]complex128, 256),
+			}
+			for x := 0; x < matrix.R; x++ {
+				for y := 0; y < matrix.C; y++ {
+					if rnd.Float64() < i {
+						row := matrix.Matrix[x]
+						if row == nil {
+							row = make(map[int]complex128)
+						}
+						row[y] = complex(rnd.Float64(), rnd.Float64())
+						matrix.Matrix[x] = row
+					}
+				}
+			}
+			matrix.Multiply(&matrix)
+		}
+		fmt.Printf("%f %v\n", i, time.Now().Sub(start))
+	}
+
+	start := time.Now()
+	for j := 0; j < 256; j++ {
+		matrix := heisenberg.Dense128{
+			R:      256,
+			C:      256,
+			Matrix: make([]complex128, 0, 256*256),
+		}
+		for i := 0; i < 256*256; i++ {
+			matrix.Matrix = append(matrix.Matrix, complex(rnd.Float64(), rnd.Float64()))
+		}
+		matrix.Multiply(&matrix)
+	}
+	fmt.Printf("%v\n", time.Now().Sub(start))
 }
